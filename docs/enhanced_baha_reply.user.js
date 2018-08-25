5a36ef20e1b4dd3ac1e504fb5478aae7dcc73867
Johnson <johnson18404@gmail.com>
2018-08-25 21:21:59 +0800

    update script

--- docs/enhanced_baha_reply.user.js
+++ docs/enhanced_baha_reply.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Enhanced Baha Reply
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Enhance Baha Forum reply function.
+// @version      1.2
+// @description  Enhance Baha Forum reply function. https://forum.gamer.com.tw/Co.php?bsn=60076&sn=49589689
 // @author       johnson18404
 // @match        https://forum.gamer.com.tw/C.php*
 // @grant        none
@@ -10,11 +10,11 @@
 
 (function() {
     'use strict';
-    var version = 1.0;
-    var $ = jQuery;
-    var g_List = [];
+    let version = 1.2;
+    let $ = jQuery;
+    let g_List = [];
 
-    var myMarkBtnCss = `
+    let myMarkBtnCss = `
         background:none!important;
         border:none;
         padding:0!important;
@@ -25,9 +25,9 @@
         margin-left: 10px;
     `;
 
-    var Record = {
+    let Record = {
         load: () => {
-            var record;
+            let record;
             record = localStorage.getItem("fool_user_id");
             if (record == null) record = [];
             else record = JSON.parse(record);
@@ -36,7 +36,7 @@
         },
         insert: (userID) => {
             // load currert
-            var record = Record.load();
+            let record = Record.load();
 
             // insert id
             if (record.includes(userID)) return false;
@@ -48,10 +48,10 @@
         },
         remove: (userID) => {
             // load currert
-            var record = Record.load();
+            let record = Record.load();
 
             // remove id
-            var index = record.indexOf(userID);
+            let index = record.indexOf(userID);
             if (index < 0) return false;
             record.splice(index, 1);
 
@@ -62,25 +62,26 @@
     };
 
 
-    var ContentVisibility = {
+    let ContentVisibility = {
       show: (elmt) => {
-          var t = $(elmt);
+          let t = $(elmt);
           $(t.find('img')[0]).css('opacity', ''); // avatar
           $(t.find('a.reply-content__user')[0]).css('color', ''); // id
           $(t.find('article.reply-content__article')[0]).css('color', ''); // link
           $(t.find('input.myMarkBtn')[0]).attr('value', 'X');
       },
       hide: (elmt) => {
-          var t = $(elmt);
+          let t = $(elmt);
           $(t.find('img')[0]).css('opacity', '0.2'); // avatar
           $(t.find('a.reply-content__user')[0]).css('color', '#8080802b'); // id
-          $(t.find('article.reply-content__article')[0]).css('color', '#8080802b'); // link
+          //   $(t.find('article.reply-content__article')[0]).css('color', '#8080802b'); // link
+          $(t.find('article.reply-content__article')[0]).css('color', '#f3f4f4'); // link
           $(t.find('input.myMarkBtn')[0]).attr('value', 'cancel');
       }
     };
 
     function MarkContent(action, crt_i, tag_userID) {
-        for (var i=crt_i-1; i>=0; i--) {
+        for (let i=crt_i-1; i>=0; i--) {
             if (g_List[i].userID.replace(/：$/, '') == tag_userID) {
                if (action == 1) {
                    $(g_List[i].elmt).attr('style', 'border: 2px solid;     border-color: red;');
@@ -93,13 +94,13 @@
     }
 
     function TagUserName(tag_i, elmt) {
-        var t = $( $(elmt).find('a.reply-content__user')[0] );
+        let t = $( $(elmt).find('a.reply-content__user')[0] );
         t.text( t.text().replace(/：$/, '') + '：' );
 
         $.each($( $(elmt).find('article.reply-content__article')[0] ).find('a'), (i, y) => {
            if ($(y).attr('onclick') == undefined) {
-               var userID = $(y).text();
-               var show = false;
+               let userID = $(y).text();
+               let show = false;
                $(y)
                    .attr('style', 'border: 1px solid;    border-color: gray;    border-radius: 5px;')
                    .mouseenter(() => {
@@ -123,22 +124,22 @@
         console.log(`Enhanced Baha Reply ver:${version}`);
 
         g_List = [];
-        var arr = $('div.c-reply__item');
-        var record = Record.load();
+        let arr = $('div.c-reply__item');
+        let record = Record.load();
 
         $.each(arr, (i, elmt) => {
             // get current userID
-            var userID = ($($(elmt).find('a.reply-content__user')[0])).text().replace(/：$/, '');
+            let userID = ($($(elmt).find('a.reply-content__user')[0])).text().replace(/：$/, '');
 
             // TagUserName
             TagUserName(i, elmt);
 
             // Append "Mark" Button
-            var btnExistCheck = $(elmt).find('input.myMarkBtn');
+            let btnExistCheck = $(elmt).find('input.myMarkBtn');
             if (btnExistCheck.length == 0) {
-                var newbtn = $(`<input type="button" class="myMarkBtn" value="X" style="${myMarkBtnCss} color: gray;"/>`);
+                let newbtn = $(`<input type="button" class="myMarkBtn" value="X" style="${myMarkBtnCss} color: gray;"/>`);
                 newbtn.click(function() {
-                    var crt_label = newbtn.attr('value');
+                    let crt_label = newbtn.attr('value');
 
                     if (crt_label == 'cancel') {
                         newbtn.attr('value', 'X');
